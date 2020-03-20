ded4bed798708f6604bee0590c5cc50ee9497755
priyaranjan singh <58334735+priyeranjansingh@users.noreply.github.com>
2020-03-20 06:09:01 -0700

    Create file.php

---
+++ file.php
@@ -0,0 +1,11 @@
+Step To remove .html from magento
+Magento comes with .html for all category and product URLs by default.
+
+If you wish to remove the .html extension from all Category URL & Product URL, this is how you do it:
+1. Login to your magento admin panel
+2. Go to System > Config > Catalog
+3. From 'Search Engine Optimizations' tab delete ".html" from 'Product URL Suffix' and 'Category URL Suffix'
+4. Save config.
+5. Then go to System > Index Management and Reindex All data
+6. Now clear your caches.
+7. Done
